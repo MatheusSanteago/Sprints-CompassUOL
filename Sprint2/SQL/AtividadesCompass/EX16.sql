57243eb63b280fb7ddc2822fcf0dc28011d9f524
MatheusSanteago <matheussanteago@gmail.com>
2023-04-03 13:40:46 -0300

    Adicionando uma resolução melhor para o EX16

--- Sprint2/SQL/AtividadesCompass/EX16.sql
+++ Sprint2/SQL/AtividadesCompass/EX16.sql
@@ -1,24 +1,3 @@
-select estado , nmpro, ROUND(ROUND(sum(qtd),4) / COUNT(*),4) as quantidade_media from tbvendas t2
-where status = 'Concluído' and nmpro LIKE '%A%'
-GROUP by estado
-UNION 
-select estado , nmpro,  ROUND(ROUND(sum(qtd),4) / COUNT(*),4) as quantidade_media from tbvendas t2
-where status = 'Concluído' and nmpro LIKE '%C%'
-GROUP by estado
-UNION 
-select estado , nmpro,  ROUND(ROUND(sum(qtd),4) / COUNT(*),4) as quantidade_media from tbvendas t2
-where status = 'Concluído' and nmpro LIKE '%CH%'
-GROUP by estado
-UNION 
-select estado , nmpro,  ROUND(ROUND(sum(qtd),4) / COUNT(*),4) as quantidade_media from tbvendas t2
-where status = 'Concluído' and nmpro LIKE '%TN%'
-GROUP by estado
-UNION 
-select estado , nmpro,  ROUND(ROUND(sum(qtd),4) / COUNT(*),4) as quantidade_media from tbvendas t2
-where status = 'Concluído' and nmpro LIKE '%SL%'
-GROUP by estado
-UNION 
-select estado , nmpro, ROUND(ROUND(sum(qtd),4) / COUNT(*),4) as quantidade_media from tbvendas t2
-where status = 'Concluído' and nmpro LIKE '%E%'
-GROUP by estado
-ORDER BY estado ;
+select estado , nmpro, ROUND(AVG(qtd),4) as quantidade_media from tbvendas
+where status = 'Concluído'
+GROUP by estado, nmpro;
