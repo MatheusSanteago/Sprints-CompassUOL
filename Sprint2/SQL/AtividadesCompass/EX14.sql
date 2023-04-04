6ff040f38192e9961bece2a16b3f596eab6b4115
MatheusSanteago <matheussanteago@gmail.com>
2023-04-04 15:38:19 -0300

    Melhorando EX14

--- Sprint2/SQL/AtividadesCompass/EX14.sql
+++ Sprint2/SQL/AtividadesCompass/EX14.sql
@@ -1,4 +1,4 @@
-select estado, ROUND(ROUND(sum(qtd * vrunt),2) / count(*),2) as gastomedio from tbvendas t2
+select estado, ROUND(AVG(qtd * vrunt),2) as gastomedio from tbvendas t2
 where status = 'Concluído'
 group by estado
 order by gastomedio desc;
