d5e54600fec997c155f0ad8ec77be06c773a91c7
MatheusSanteago <matheussanteago@gmail.com>
2023-04-05 16:33:56 -0300

    Atualizando query

--- Sprint2/SQL/AtividadesCompass/EX4.sql
+++ Sprint2/SQL/AtividadesCompass/EX4.sql
@@ -1,4 +1,4 @@
-SELECT a.nome,codautor,nascimento, count(l.titulo) as quantidade FROM livro as l
+SELECT nome,codautor,nascimento, count(l.titulo) as quantidade FROM livro as l
 FULL JOIN autor as a ON a.codautor = l.autor
-GROUP BY a.nome
+GROUP BY nome
 ORDER BY nome;
