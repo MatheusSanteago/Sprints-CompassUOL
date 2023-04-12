66484a766b7c9a02bc0acbb2e8457346d7898779
MatheusSanteago <matheussanteago@gmail.com>
2023-04-12 15:43:57 -0300

    Resolução atividade 18

---
+++ Sprint3/EX18.py
@@ -0,0 +1,8 @@
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44,
+         'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+new_list = []
+
+for i, campo in enumerate(speed):
+    new_list.append(speed[campo])
+
+print(list(set(new_list)))
