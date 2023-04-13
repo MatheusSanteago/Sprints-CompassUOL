88cc46120e2a5e6289a78fc5554bc669941ade4f
MatheusSanteago <matheussanteago@gmail.com>
2023-04-13 11:19:15 -0300

    Resolução atividade 14

--- Sprint3/EX14.py
+++ Sprint3/EX14.py
@@ -1,10 +1,8 @@
-def t(a, b, c, d, parametro_nomeado='alguma coisa', x=20):
-    print(a)
-    print(b)
-    print(c)
-    print(d)
-    print(parametro_nomeado)
-    print(x)
+def t(*args, **kwargs):
+    for i in args:
+        print(i)
+    for i in kwargs.values():
+        print(i)
 
 
 t(1, 3, 4, 'hello', parametro_nomeado='alguma coisa', x=20)
