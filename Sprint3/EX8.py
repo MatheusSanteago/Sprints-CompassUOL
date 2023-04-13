e7efe27db5306b52195c47b2fa398cdc60438ae6
MatheusSanteago <matheussanteago@gmail.com>
2023-04-13 10:48:17 -0300

    Alterando resolução

--- Sprint3/EX8.py
+++ Sprint3/EX8.py
@@ -1,11 +1,9 @@
 palavras = ['maça', 'arara', 'audio', 'radio', 'radar', 'moto']
 
-for j in range(len(palavras)):
-    palavra = palavras[j]
-    for i in range(len(palavra)):
-        if i == 0:
-            if palavras[j] == palavra[::-1]:
-                print(('A palavra: {} é um palíndromo').format(palavras[j]))
-            else:
-                print(('A palavra: {} não é um palíndromo').format(
-                    palavras[j]))
+for i in range(len(palavras)):
+    if i == 0:
+        if palavras[i] == palavras[::-1]:
+            print(('A palavra: {} é um palíndromo').format(palavras[i]))
+    else:
+        print(('A palavra: {} não é um palíndromo').format(
+            palavras[i]))
