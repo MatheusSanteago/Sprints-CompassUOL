fedc95e0f59c6d1116b6c64ef808bf776675071d
MatheusSanteago <matheussanteago@gmail.com>
2023-04-13 21:28:24 -0300

    Resolução atividade 22

---
+++ Sprint3/EX22.py
@@ -0,0 +1,17 @@
+class Pessoa:
+    def __init__(self, id):
+        self._nome = 0
+        self.id = id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, a):
+        self._nome = a
+
+
+pessoa = Pessoa(0)
+pessoa.nome = "Fulano de Tal"
+print(pessoa.nome)
