c3cc945f3a7d91c63d31ae4d7affe97f2da9e740
Matheus <matheussanteago@gmail.com>
2023-04-17 19:05:10 -0300

    Atualizando resolução

--- Sprint3/EX25.py
+++ Sprint3/EX25.py
@@ -4,13 +4,10 @@ class Aviao():
         self.velocidade_maxima = velocidade_maxima
         self.cor = 'Azul'
         self.capacidade = capacidade
+        print(
+            f'O avião de modelo {self.modelo} possui uma velocidade máxima de {self.velocidade_maxima},apacidade para {self.capacidade} passageiros e é da cor {self.cor}.')
 
 
-boeing = Aviao('BOIENG456', 1500, 400)
-embraer = Aviao('Embraer Praetor 600', 863, 14)
-antonov = Aviao('Antonov An-2', 258, 12)
-
-avioes = [boeing, embraer, antonov]
-
-for i in range(len(avioes)):
-    print(f'O avião de modelo {avioes[i].modelo} possui uma velocidade máxima de {avioes[i].velocidade_maxima}, capacidade para {avioes[i].capacidade} passageiros e é da cor {avioes[i].cor}.')
+Aviao('BOIENG456', 1500, 400)
+Aviao('Embraer Praetor 600', 863, 14)
+Aviao('Antonov An-2', 258, 12)
