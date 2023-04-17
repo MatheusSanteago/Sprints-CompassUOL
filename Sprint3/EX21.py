016bbde173d84f19d1a402ded1a1f320c1990c3e
MatheusSanteago <matheussanteago@gmail.com>
2023-04-17 17:17:18 -0300

    Alterando resolução

--- Sprint3/EX21.py
+++ Sprint3/EX21.py
@@ -1,26 +1,25 @@
 class Passaro():
-    def __init__(self):
-        pass
+    def __init__(self, especie, som):
+        self.especie = especie
+        self.som = som
 
     def emitirSom(self):
-        print(f'{self._especie} emitindo som...')
-        print(self._som)
+        print(f'{self.especie} emitindo som...')
+        print(self.som)
 
     def voar(self):
-        print(self._especie)
+        print(self.especie)
         print('Voando...')
 
 
-class Pato(Passaro):
-    def __init__(self):
-        self._especie = 'Pato'
-        self._som = 'Quack Quack'
-
-
 class Pardal(Passaro):
-    def __init__(self):
-        self._especie = 'Pardal'
-        self._som = 'Piu Piu'
+    def __init__(self, especie='Pardal', som='Piu Piu'):
+        super().__init__(especie, som)
+
+
+class Pato(Passaro):
+    def __init__(self, especie='Pato', som='Quack Quack'):
+        super().__init__(especie, som)
 
 
 pato = Pato()
