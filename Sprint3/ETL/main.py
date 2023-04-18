844229e4a861be4200d10cdae1d148ac74fd4102
MatheusSanteago <matheussanteago@gmail.com>
2023-04-18 00:49:08 -0300

    Mudan;as no Transform

--- Sprint3/ETL/main.py
+++ Sprint3/ETL/main.py
@@ -1,7 +1,3 @@
-from fnmatch import translate
-import tracemalloc
-
-
 file = 'actors.csv'
 actorsGross = []
 
@@ -14,14 +10,26 @@ def extracao(file, data):   # Extração
 
 
 def transformacao(data):  # Transformação
+    data.pop(0)
     for i in range(len(data)):
+        if len(data[i]) >= 7:
+            data[i][0] = data[i][0] + data[i][1]
+            data[i].pop(1)
         for x in range(len(data[i])):
             if data[i][x][0].isnumeric():
-                data[i][x] = float((data[i][x]).replace(' ', ''))
+                if x == 2:
+                    data[i][x] = int((data[i][x]).replace(
+                        ' ', ''))
+                else:
+                    data[i][x] = float((data[i][x]).replace(
+                        ' ', ''))
+
     return data
 
 
 if __name__ == '__main__':
     extracao(file, actorsGross)
     actors = transformacao(actorsGross)
-    print(actors)
+    # for i in actors:
+    #     print(i[2] is int)
+    # Resultados
