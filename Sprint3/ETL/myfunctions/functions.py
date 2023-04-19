5071ea3b4bc804e3711b11a12075caf239141a95
MatheusSanteago <matheussanteago@gmail.com>
2023-04-19 17:07:45 -0300

    Alterando arquivo de funções

--- Sprint3/ETL/myfunctions/functions.py
+++ Sprint3/ETL/myfunctions/functions.py
@@ -1,5 +1,5 @@
 def myMax(data, y, n):  # Função com a resolução das atividades 1 e 2
-    f = open(f'etapa-{n}.txt', 'w')
+    f = open(f'./RespostasTXT/etapa-{n}.txt', 'w')
 
     axis_num = [data[x][y] for x in range(len(data)) if data[x][y] > 0]
     i = axis_num.index(max(axis_num))
@@ -10,7 +10,6 @@ def myMax(data, y, n):  # Função com a resolução das atividades 1 e 2
         content = f'O ator/atriz {data[i][0]} obteve a maior média de \
 faturamento por filme, equivalente a {data[i][y]} \n'
         f.write(content)
-
     f.close()
 
 
@@ -19,19 +18,18 @@ def avgBruto(a):
 
 
 def avgFaturamentoBruto(data):  # Função atividade 3
-    f = open(f'etapa-{3}.txt', 'w')
+    f = open(f'./RespostasTXT/etapa-{3}.txt', 'w')
 
     aux = list(map(avgBruto, data))
     for i in range(len(data)):
         content = f'Ator/Atriz: {data[i][0]} -- Média \
 de Faturamento bruto: {aux[i]} \n'
         f.write(content)
-
     f.close()
 
 
 def filmeFrequente(data):  # Função atividade 4
-    f = open(f'etapa-{4}.txt', 'w')
+    f = open(f'./RespostasTXT/etapa-{4}.txt', 'w')
 
     movies = [data[i][4] for i in range(len(data))]
     mostWatch = {
@@ -42,18 +40,15 @@ def filmeFrequente(data):  # Função atividade 4
         counter = movies.count(movies[i])
         if counter > mostWatch['frequency']:
             mostWatch['frequency'], mostWatch['movie'] = counter, movies[i]
-        else:
-            pass
         if i == len(data) - 1:
             text = f'O filme mais frequente é {mostWatch["movie"]} com \
-{mostWatch["frequency"] + 1} frequências'
+{mostWatch["frequency"]} frequências'
             f.write(text)
-
     f.close()
 
 
 def maiorFaturamento(data):  # Função atividade 5
-    f = open(f'etapa-{5}.txt', 'w')
+    f = open(f'./RespostasTXT/etapa-{5}.txt', 'w')
 
     aux = sorted([data[i][1] for i in range(len(data))], reverse=True)
     for i in range(len(aux)):
