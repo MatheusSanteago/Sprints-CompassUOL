36714474d6aec5002624b6d6a87bc000be132048
MatheusSanteago <matheussanteago@gmail.com>
2023-04-25 16:04:18 -0300

    Resolução atividade 1

---
+++ Sprint4/EX1.py
@@ -0,0 +1,25 @@
+# E1
+# map()
+# filter() OK
+# sorted() OK
+# sum() OK
+# Seu código deverá exibir na saída(simplesmente utilizando 2 comandos 'print()'):
+# a lista dos 5 maiores números pares em ordem decrescente
+# a soma destes valores.
+
+file = 'number.txt'
+numList = []
+
+with open(file) as numbers:
+    try:
+        for num in numbers:
+            numList.append(num.rstrip('\n'))
+    finally:
+        numList = list(map(int, numList))
+
+
+maioresPares = list(filter(lambda n: n % 2 == 0, numList))
+cincoMaiores = sorted((maioresPares), reverse=True)[:5]
+
+print(cincoMaiores)
+print(sum(cincoMaiores))
