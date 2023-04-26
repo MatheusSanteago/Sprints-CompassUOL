497330eec14e436ab2e828e4efd447c8b383e55c
MatheusSanteago <matheussanteago@gmail.com>
2023-04-26 11:57:30 -0300

    Resolução atividade 4

---
+++ Sprint4/EX4.py
@@ -0,0 +1,30 @@
+operadores = ['+', '-', '*', '/', '+']
+operandos = [(3, 6), (-7, 4.9), (8, -8), (10, 2), (8, 4)]
+
+
+def junt(a, b, c):
+    match b:
+        case '+':
+            return a + c
+        case '-':
+            return a - c
+        case '*':
+            return a * c
+        case '/':
+            return a / c
+        case '%':
+            return a % c
+        case _:
+            pass
+
+
+def calcular_valor_maximo(operadores, operandos) -> float:
+    t = tuple(zip(operadores, operandos))
+    num1 = list((map(lambda a: a[1][0], t)))
+    num2 = list((map(lambda a: a[1][1], t)))
+    listaCalculada = list(map(junt, num1, operadores, num2))
+    maxValue = max(listaCalculada)
+    return maxValue
+
+
+print(calcular_valor_maximo(operadores, operandos))
