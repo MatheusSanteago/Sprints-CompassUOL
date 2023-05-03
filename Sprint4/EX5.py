a5b15e7d718cf8132f4209b8743cbc5d67b364ca
MatheusSanteago <matheussanteago@gmail.com>
2023-05-03 16:52:00 -0300

    Alterando resolução da EX5

--- Sprint4/EX5.py
+++ Sprint4/EX5.py
@@ -1,22 +1,17 @@
-from statistics import mean
-
 file = 'estudantes.csv'
-lista = []
+listaAlunos = []
 
 
 with open(file) as alunos:
-    for aluno in alunos:
-        lista.append(aluno.rstrip('\n').split(','))
-
-listaAlunos = sorted(lista)
-listaAlunos.sort()
-alunos = list(map(lambda a: a[0], listaAlunos))
-notasStr = list(map(lambda a: a[1:], listaAlunos))
-melhoresNotas = [list((map(int, x))) for x in notasStr]
-
+    try:
+        for aluno in alunos:
+            listaAlunos.append(aluno.rstrip('\n').split(','))
+    finally:
+        listaAlunos.sort()
+        notasStr = list(map(lambda a: a[1:], listaAlunos))
+        melhoresNotas = [list((map(int, x))) for x in notasStr]
 
 for x in range(len(alunos)):
-    nome = alunos[x]
     notas = sorted(melhoresNotas[x], reverse=True)[0:3]
-    media = float(round(mean(notas), 2))
-    print(f"Nome: {nome} Notas: {notas} Média: {media}")
+    media = round(sum(notas) / 3, 2)
+    print(f"Nome: {listaAlunos[x][0]} Notas: {notas} Média: {media}")
