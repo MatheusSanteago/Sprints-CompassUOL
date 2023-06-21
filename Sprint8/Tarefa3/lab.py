630a859f14efdca411132e8ff043c1e5aacb3e1a
MatheusSanteago <matheussanteago@gmail.com>
2023-06-21 15:21:16 -0300

    Script para gerar nomes aleatórios

---
+++ Sprint8/Tarefa3/lab.py
@@ -0,0 +1,25 @@
+import names
+import random
+import time
+
+random.seed(40)
+qtd_nomes_unicos = 3000
+qtd_nomes_aleatorios = 10000000
+
+aux = []
+
+for i in range(0, qtd_nomes_unicos):
+    aux.append(names.get_full_name())
+
+print("Gerando {} nomes aleatórios".format(qtd_nomes_aleatorios))
+
+dados = []
+
+for i in range(0, qtd_nomes_aleatorios):
+    dados.append(random.choice(aux))
+
+f = open('nomes_aleatorios.txt', 'x')
+for i in dados:
+    f.write(f'{i}\n')
+f.close()
+
