fac1a9abeb543bd64c096efd1899d6e9f85d2980
MatheusSanteago <matheussanteago@gmail.com>
2023-06-21 16:51:39 -0300

    Alterando código

--- Sprint8/Desafio-ParteII/main.py
+++ Sprint8/Desafio-ParteII/main.py
@@ -8,6 +8,7 @@ from botocore.exceptions import ClientError
 ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
 SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 SESSION_TOKEN = os.environ['AWS_SESSION_TOKEN']
+API_KEY = os.environ['API_KEY_TMDB'] 
 
 session = boto3.Session(
     aws_access_key_id=ACCESS_KEY,
@@ -20,8 +21,7 @@ s3 = session.client('s3')
 
 actorsList = [500, 976, 12835, 18918, 6384, 18897, 62, 1100,
               16483, 1245, 10205, 8691, 1339, 17647, 63, 11701, 139, 2713]
-actorsMovies = []
-actorsSeries = []
+actorsMedia = []
 
 
 def lambda_handler(event, context):
@@ -36,25 +36,20 @@ def lambda_handler(event, context):
 
         microBatchData()
 
-        categories = [{"type": "Movies", "list": actorsMovies},
-                      {"type": "Series", "list": actorsSeries}]
+        file = "RAW/TMDB/JSON/{}/{}/{}/{}".format(
+            date.year, date.month, date.day, f"actorsAction.json")
 
-        for i in range(0, 2):
-            fileLower = categories[i]["type"].lower()
-            file = "RAW/TMDB/JSON/{}/{}/{}/{}/{}".format(
-                categories[i]["type"], date.year, date.month, date.day, f"{fileLower}.json")
-            try:
-                print(f"Fazendo Upload em {file}")
-                s3.put_object(Body=json.dumps(categories[i]["list"], ensure_ascii=False).encode(
-                    'utf8'), Bucket='pblabum', Key=file)
-            finally:
-                print(f"{i + 1} Upload finalizado")
+        try:
+            print(f"Fazendo Upload em {file}")
+            s3.put_object(Body=json.dumps(actorsMedia, ensure_ascii=False).encode(
+                'utf8'), Bucket='pblabum', Key=file)
+        finally:
+            print(f"Upload finalizado")
 
 
 def req(id):
     try:
-        api_key = "721b346f73c11fd9715c5f8e5a5561bd"
-        url = f"https://api.themoviedb.org/3/person/{id}/combined_credits?api_key={api_key}&language=pt-BR"
+        url = f"https://api.themoviedb.org/3/person/{id}/combined_credits?api_key={API_KEY}&language=pt-BR"
         response = requests.get(url)
         data = response.json()
     finally:
@@ -67,11 +62,8 @@ def microBatchData():
         data = req(actorsList[i])
 
         for media in data["cast"]:
-            if media['media_type'] == "movie":
-                data = media["id_actor"] = actorsList[i]
-                actorsMovies.append(media)
-            elif media['media_type'] == "tv":
-                actorsSeries.append(media)
+            data = media["id_actor"] = actorsList[i]
+            actorsMedia.append(media)
 
 
 if __name__ in '__main__':
