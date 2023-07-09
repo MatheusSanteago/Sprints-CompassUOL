14bf2521f20b524ac755a549de9b8f4b41e0e075
MatheusSanteago <matheussanteago@gmail.com>
2023-07-09 14:20:37 -0300

    Colocando Ids remanescentes

--- Sprint8/Desafio-ParteII/main.py
+++ Sprint8/Desafio-ParteII/main.py
@@ -12,15 +12,15 @@ session = boto3.Session()
 date = datetime.datetime.now()
 s3 = session.client('s3')
 
-actorsList = [1620, 63, 193, 11701, 17647, 1339, 8691, 10205, 1245, 2713, 500, 976, 12835, 18918, 6384, 18897, 62, 1100,
+actorsList = [1620, 63, 103, 11701, 17647, 1339, 8691, 10205, 1245, 2713, 500, 976, 12835, 18918, 6384, 18897, 62, 1100,
               16483, 15111, 139]
 actorsMedia = []
 
 
 def lambda_handler(event, context):
     try:
-      response = s3.list_buckets()
-      print(response)
+        response = s3.list_buckets()
+        print(response)
     except ClientError as e:
         print("Error\n")
         print(e)
@@ -28,12 +28,13 @@ def lambda_handler(event, context):
         print("Conectado ao S3\n Iniciando Batch ")
         microBatchData()
         file = f"RAW/TMDB/JSON/{date.year}/{date.month}/{date.day}/actorsAction.json"
-        
+
         try:
             print(f"Fazendo Upload em {file}")
-            s3.put_object(Body=json.dumps(actorsMedia, ensure_ascii=False).encode('utf8'), Bucket='pblabum', Key=file)
+            s3.put_object(Body=json.dumps(actorsMedia, ensure_ascii=False).encode(
+                'utf8'), Bucket='pblabum', Key=file)
         finally:
-            print(f"Upload finalizado") 
+            print(f"Upload finalizado")
 
 
 def req(id):
@@ -52,8 +53,7 @@ def microBatchData():
 
         for media in data:
             actorsMedia.append(data)
-           
+
 
 if __name__ in '__main__':
     lambda_handler()
-    
